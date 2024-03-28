a06d53d6013d7493237d2f2a862f66450eed71b6
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-28 18:01:20 +0100

    Add forms for authors and quotes

--- quotes_project/quotesapp/forms.py
+++ quotes_project/quotesapp/forms.py
@@ -1,5 +1,6 @@
-from django.forms import ModelForm, CharField, TextInput
-from .models import Tag
+from django.forms import ModelForm, CharField, DateField
+from django.forms import TextInput, ModelChoiceField, ModelMultipleChoiceField, CheckboxSelectMultiple
+from .models import Tag, Author, Quote
 
 
 class TagForm(ModelForm):
@@ -11,3 +12,36 @@ class TagForm(ModelForm):
     class Meta:
         model = Tag
         fields = ['name']
+
+
+class AuthorForm(ModelForm):
+
+    fullname = CharField(
+        min_length=3, max_length=70, required=True, widget=TextInput()
+    )
+    birth_date = DateField()
+    birth_location = CharField(
+        min_length=5, max_length=150, required=True, widget=TextInput()
+    )
+    description = CharField()
+
+    class Meta:
+        model = Author
+        fields = ['fullname', 'birth_date', 'birth_location', 'description']
+
+
+class QuoteForm(ModelForm):
+
+    quote = CharField(
+        min_length=3, max_length=255, required=True, widget=TextInput()
+    )
+    author = ModelChoiceField(queryset=Author.objects.all())
+    tags = ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=CheckboxSelectMultiple(),
+        required=False
+    )
+
+    class Meta:
+        model = Quote
+        fields = ["quote", "author", "tags"]
