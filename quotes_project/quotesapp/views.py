b3d87e527f94e469ba276e3cfd58106e20506e32
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-28 20:24:00 +0100

    Update

--- quotes_project/quotesapp/views.py
+++ quotes_project/quotesapp/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import get_list_or_404, get_object_or_404
+from django.shortcuts import get_object_or_404
+from django.core.paginator import PageNotAnInteger, EmptyPage
 from django.core.paginator import Paginator
 from .models import Quote, Author, Tag
 from .forms import TagForm, AuthorForm, QuoteForm
+from django.contrib import messages
 
-
-def main(request, page=1):
-    quotes = get_list_or_404(Quote.objects.all())
+def main(request):
+    quotes_list = Quote.objects.all()
     elem_per_page = 10
-    paginator = Paginator(quotes, elem_per_page)
-    quotes_on_page = paginator.page(page)
+    paginator = Paginator(quotes_list, elem_per_page)
+
+    page = request.GET.get('page')
+
+    try:
+        quotes = paginator.page(page)
+    except PageNotAnInteger:
+        quotes = paginator.page(1)
+    except EmptyPage:
+        quotes = paginator.page(paginator.num_pages)
+
     context = {
-        'quotes': quotes_on_page
+        'quotes': quotes
     }
     return render(request, 'index.html', context=context)
 
@@ -51,18 +61,41 @@ def edit_author(request, author_id):
         form = AuthorForm(request.POST, instance=author)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Author updated successfully.')
             return redirect('quotesapp:author_detail', author_id=author.id)
+        else:
+            messages.error(request, 'Error updating author.')
     else:
         form = AuthorForm(instance=author)
     return render(request, 'edit_author.html', {'form': form, 'author': author})
 
 
 def add_quote(request):
+    tags = Tag.objects.all()
+
     if request.method == 'POST':
         form = QuoteForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_quote = form.save()
+
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                new_quote.tags.add(tag)
+
             return redirect(to='quotesapp:index')
         else:
-            return render(request, 'quote_form.html', {'form': form})
-    return render(request, 'quote_form.html', {'form': QuoteForm()})
+            return render(request, 'quote_form.html', {"tags": tags, 'form': form})
+
+    return render(request, 'quote_form.html', {"tags": tags, 'form': QuoteForm()})
+
+
+def edit_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+    if request.method == 'POST':
+        form = QuoteForm(request.POST, instance=quote)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotesapp:index')
+    else:
+        form = QuoteForm(instance=quote)
+    return render(request, 'edit_quote.html', {'form': form})
