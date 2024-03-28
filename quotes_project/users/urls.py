b711febd14499ae8ddca6428a8d9166236485a9e
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-28 21:04:14 +0100

    Add user logging and logging out

--- quotes_project/users/urls.py
+++ quotes_project/users/urls.py
@@ -5,4 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.signupuser, name='signup'),
+    path('login/', views.loginuser, name='login'),
+    path('logout/', views.logoutuser, name='logout'),
 ]
--- quotes_project/users/views.py
+++ quotes_project/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-
-from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RegisterForm, LoginForm
 
 
 def signupuser(request):
@@ -16,3 +18,25 @@ def signupuser(request):
             return render(request, 'signup.html', context={"form": form})
 
     return render(request, 'signup.html', context={"form": RegisterForm()})
+
+
+def loginuser(request):
+    # if request.user.is_authenticated:
+    #     return redirect(to='quoteapp:index')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='quoteapp:index')
+
+    return render(request, 'login.html', context={"form": LoginForm()})
+
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='noteapp:main')
