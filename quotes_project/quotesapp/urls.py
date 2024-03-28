06adb479ab4ed54ef3f7cef5155f59ef6d09b795
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-28 18:00:40 +0100

    Add paths

--- quotes_project/quotesapp/urls.py
+++ quotes_project/quotesapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'quotesapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main, name='index'),
+    path('tag/', views.add_tag, name='add_tag'),
+    path('author_form/', views.add_author, name='add_author'),
+    path('author/<int:author_id>', views.author_detail, name='author_detail'),
+    path('quote_form/', views.add_quote, name='add_quote'),
 ]
