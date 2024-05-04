fab49530aceee61afdfb122382bf827af0a74897
Aleksander <alex.nuclearboy@gmail.com>
2024-05-04 12:50:21 +0200

    Add documentation

--- quotes_project/users/views.py
+++ quotes_project/users/views.py
@@ -6,26 +6,35 @@ from .forms import RegisterForm, LoginForm, ProfileForm
 
 
 def signupuser(request):
-    # if request.user.is_authenticated:
-    #     return redirect(to='quotesapp:index')
-
+    """
+    Handle user registration.
+    If POST request, processes form data to register a new user. Redirects to 
+    index page upon successful registration.
+    If GET request, displays the registration form.
+    """
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect(to='quotesapp:index')
         else:
-            return render(request, 'users/signup.html', context={"form": form})
+            return render(request, 'users/signup.html',
+                          context={"form": form})
 
-    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+    return render(request, 'users/signup.html',
+                  context={"form": RegisterForm()})
 
 
 def loginuser(request):
-    # if request.user.is_authenticated:
-    #     return redirect(to='quoteapp:index')
-
+    """
+    Handle user login.
+    If POST request, authenticates user. If authentication fails,
+    returns to login page with an error message. If successful,
+    redirects to the index page.
+    """
     if request.method == 'POST':
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(username=request.POST['username'],
+                            password=request.POST['password'])
         if user is None:
             messages.error(request, 'Username or password didn\'t match')
             return redirect(to='users:login')
@@ -38,18 +47,30 @@ def loginuser(request):
 
 @login_required
 def logoutuser(request):
+    """
+    Log out the current user and redirect to the index page.
+    """
     logout(request)
     return redirect(to='quotesapp:index')
 
 
 @login_required
 def profile(request):
+    """
+    Handle user profile updates.
+    If POST request, processes form data to update user's profile.
+    If successful, redirects to the profile page with a success message.
+    If GET request, displays the user's profile form.
+    """
     if request.method == 'POST':
-        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        profile_form = ProfileForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if profile_form.is_valid():
             profile_form.save()
             messages.success(request, 'Your profile is updated successfully')
             return redirect(to='users:profile')
 
     profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'users/profile.html', {'profile_form': profile_form})
+    return render(request, 'users/profile.html',
+                  {'profile_form': profile_form})
